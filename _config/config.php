6692abb2dd03cd7abe07664371ab56f982e98532
Josh Knight <jknight@legacy.com>
2015-07-22 13:12:25 -0400

    Updated config admin email for production

--- _config/config.php
+++ _config/config.php
@@ -2,6 +2,6 @@
 
 define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/menu-manager');
 define('WEB_ROOT', '/menu-manager');
-// define('GREEN_HEART_FOODS_ADMIN_EMAIL', 'admin@greenheartfoods.com');
-define('GREEN_HEART_FOODS_ADMIN_EMAIL', 'josh@seven-seventeen.com');
+define('GREEN_HEART_FOODS_ADMIN_EMAIL', 'admin@greenheartfoods.com');
+// define('GREEN_HEART_FOODS_ADMIN_EMAIL', 'josh@seven-seventeen.com');
 define('ALLERGY_ALERT_ARRAY', 'contains_gluten, contains_dairy, contains_nuts, contains_nightshades, contains_alcohol, contains_eggs, contains_shellfish');
